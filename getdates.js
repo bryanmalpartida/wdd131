7e1a1754374c4592d3e2bb1365789de027dbc2a6
bryanmalpartida <bmalpartidatrelles@byupathway.edu>
2025-01-14 19:10:52 -0500

    update

---
+++ getdates.js
@@ -0,0 +1,23 @@
+//LAST MODIFIED
+
+// giving variable to last modified
+const lastModifiedDate = document.lastModified;
+
+// using another variable so we can call lastmodified by id
+const lastModifiedParagraph = document.getElementById('lastModified');
+
+// desired text+outcome
+lastModifiedParagraph.textContent = `Last Modification: ${lastModifiedDate}`;
+
+//YEAR 
+
+//giving variable to year
+const year = document.querySelector("#year");
+//variable for current year 
+const today = new Date();
+//print
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+const studentname = document.getElementById('studentname');
+
+studentname.innerHTML = 'WDD 131: Dynamic Web Fundamentals - <span class="studentname">Bryan Malpartida</span>';

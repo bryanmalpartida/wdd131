7efa6049cd0627cc0bd23e4e8125590d85783452
bryanmalpartida <bmalpartidatrelles@byupathway.edu>
2025-02-08 19:23:33 -0500

    u

--- scripts/place.js
+++ scripts/place.js
@@ -17,3 +17,44 @@ const year = document.querySelector("#year");
 const today = new Date();
 //print
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
+
+
+//WINDCHILL START
+
+
+function calculateWindChill(temperature, windSpeed, unit = 'C') {
+   
+    if (
+        (unit === 'C' && temperature <= 10 && windSpeed > 4.8) ||
+        (unit === 'F' && temperature <= 50 && windSpeed > 3)
+    ) {
+        
+        if (unit === 'C') {
+           
+            return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)).toFixed(2) + ' °C';
+        } else {
+            
+            return (35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16)).toFixed(2) + ' °F';
+        }
+    } else {
+       
+        return 'N/A';
+    }
+}
+
+
+const temperature = 18; 
+const windSpeed = 10; 
+
+const windChill = calculateWindChill(temperature, windSpeed, 'C');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const windChillElement = document.getElementById('windChill');
+    if (windChillElement) {
+        windChillElement.textContent = windChill; 
+    }
+});
+
+//WINDCHILL END
